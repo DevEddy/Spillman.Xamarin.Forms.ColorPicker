049692635fa1aba13beeae91982119887e21332b
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2021-05-21 22:47:59 +0200

    formatting

--- source/ColorPicker/ColorExtensions.cs
+++ source/ColorPicker/ColorExtensions.cs
@@ -7,9 +7,9 @@ namespace Spillman.Xamarin.Forms.ColorPicker
     {
         internal static string ToRgbHex(this Color color)
         {
-            var r = (byte)Math.Round(color.R * byte.MaxValue);
-            var g = (byte)Math.Round(color.G * byte.MaxValue);
-            var b = (byte)Math.Round(color.B * byte.MaxValue);
+            var r = (byte) Math.Round(color.R * byte.MaxValue);
+            var g = (byte) Math.Round(color.G * byte.MaxValue);
+            var b = (byte) Math.Round(color.B * byte.MaxValue);
 
             return $"{r:X2}{g:X2}{b:X2}";
         }
